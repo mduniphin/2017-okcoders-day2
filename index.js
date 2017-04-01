005772ea49b1e93cec6281a22141c3a1af7b97c1
mduniphin <maxx.duniphin@gmail.com>
2017-04-01 12:09:46 -0500

    Inital commit

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var name = "Princess Brunhilda von Nay Nay III"; 
 var age = 8;
+var tmpAge;
+var drinkingAge;
 
 console.log("My name is " + name + " and I am age " + age);
 
-// Need to find out drinking age
-var drinkingAge = 21 - age;
+var counter = 1;
 
-if ( age < 21 ) {
-	console.log("...and beer in " + drinkingAge + " years");
-} else if (age === 21){
-	console.log("yeah... celebrate");
-} else {
-	console.log("leave me alone I am drinking");
-};
+for (var counter = 1; age <= (21 - counter); counter++){
+
+// while(age <= (21 - counter)){
+	tmpAge = age + counter;
+	console.log("In " + counter + " I will be " + tmpAge);
+	// counter = counter + 1;
+
+	drinkingAge = 21 - tmpAge;
+
+	if ( tmpAge < 21 ) {
+		console.log("...and beer in " + drinkingAge + " years");
+	} else if (tmpAge === 21){
+		console.log("yeah... celebrate");
+	} else {
+		console.log("leave me alone I am drinking");
+	}
+}
